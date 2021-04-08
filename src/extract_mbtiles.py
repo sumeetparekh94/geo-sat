9ed441f97b94299924a18cad554d76c24c029e05
sumeetparekh94 <sumeet.parekh1@gmail.com>
2021-04-08 10:17:49 -0400

    completed write_to_file method to extract tiles

--- src/extract_mbtiles.py
+++ src/extract_mbtiles.py
@@ -3,43 +3,53 @@ import sqlite3
 import pickle
 import os
 import pandas as pd
-sys.path.insert(0, 'utils/helper_methods.py')
+import logging
+from helper_methods import HelperMethods
+
 
 class ExtractMBTiles(object):
     
+    def __init__(self):
+        self.omt_dir_name = 'OMTTiles'
+    
     
     def write_to_file(self, tile_path):
+        """Method to extract .mbtiles file and write image bytes to individual files(.png).
+
+        Args:
+            tile_path (string): path to the .mbtiles file
+        """
         
+        # Connect to the mbtiles file using sqlite
         connection = sqlite3.connect(tile_path)
         cursor = connection.cursor()
         query = 'SELECT * from tiles'
         cursor.execute(query)
+        
+        
         # Iterate through the mbtiles data
         for row in cursor:
-            print(row)
+            
             tzoom = row[0]
             xtile = row[1]
             ytile = row[2]
-            if tzoom > 4:
+            
+            # Provide best resolution image which would be 13 for OpenMapTiles
+            if tzoom == 13:
 
-                HelperMethods().setDir('OMTTiles')
-                exit()
-                # self.setDir(state)
-                # self.setDir(str(tzoom))
-                # self.setDir(str(xtile))
+                HelperMethods().setDir(self.omt_dir_name)
+                HelperMethods().setDir(str(tzoom))
+                HelperMethods().setDir(str(xtile))
 
-        #         with open(str(ytile) + '.png', 'wb') as output_file:
-        #             output_file = open(str(ytile) + '.png', 'wb')
-        #             output_file.write(row[3])
-        #             output_file.close()
+                with open(str(ytile) + '.png', 'wb') as output_file:
+                    output_file = open(str(ytile) + '.png', 'wb')
+                    output_file.write(row[3])
+                    output_file.close()
+                
+                os.chdir('..')
+                os.chdir('..')
+                os.chdir('..')
                     
-        #         os.chdir('..')
-        #         os.chdir('..')
-        #         os.chdir('..')
-        #         os.chdir('..')
     
-        # print('------------Writing Image to Individual Files for ' + state + ' Complete------------')
+        logging.infor('------------Writing Image to Individual Files Complete------------')
         
-    
-if __name__ == '__main__':
-    ExtractMBTiles().write_to_file('../data/maptiler-satellite-2017-11-02-us_massachusetts.mbtiles')
