06d8d7bafcf024c097f15a52f390ac2765289e3d
sumeetparekh94 <sumeet.parekh1@gmail.com>
2021-04-08 13:10:30 -0400

    added extract mbtiles and georeference tasks to driver

--- src/driver.py
+++ src/driver.py
@@ -1,2 +1,29 @@
+import os
+import sqlite3
+import pickle
+import os
+import pandas as pd
+import logging
+from extract_mbtiles import ExtractMBTiles
+from georeference_image import GeoreferenceImage
+
+
 class Driver(object):
+    
+    def __init__(self):
+        self.mbtile_path = '../data/massachusetts.mbtiles'
+    
+    def run_driver(self):
+        
+        # Extract .mbtiles and write image bytes to individual files
+        ExtractMBTiles().write_to_file(self.mbtile_path)
+        
+        # Georeference the individually extracted image files
+        GeoreferenceImage().assign_coordinates_to_image()
+        
+        
+        
+
+if __name__ == '__main__':
+    Driver().run_driver()
     
