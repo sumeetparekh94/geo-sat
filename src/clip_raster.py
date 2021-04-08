c5d5ca774e9b6cb9fb0eb39c7fc5cfb6b64e50ef
sumeetparekh94 <sumeet.parekh1@gmail.com>
2021-04-08 16:31:32 -0400

    working version of clip raster

--- src/clip_raster.py
+++ src/clip_raster.py
@@ -3,7 +3,6 @@ import geopandas as gpd
 import rasterio
 import pickle
 import json
-import psycopg2
 import pycrs
 import shapely.wkt
 from osgeo import gdal
@@ -12,39 +11,47 @@ from psycopg2.extras import RealDictCursor
 from shapely.geometry import mapping, shape, Point, MultiPolygon, Polygon
 from shapely.geometry import box
 from fiona.crs import from_epsg
+from helper_methods import HelperMethods
+
 
 class ClipRaster(object):
+    
+    def __init__(self):
+        self.stitched_file_path = '../data/mosaic.tif'
     
     
     def clip_aoi(self, geojson, output_file_name):
         
-        stitched_file = rasterio.open(os.path.join(self.stitched_file_path, output_file_name + '.tif'))
-    
-        coordinates = geojson['geometry']
-
-        coords = [shape(coordinates)]
+        stitched_file = rasterio.open(self.stitched_file_path)
+        
+        # epsg_code = int(stitched_file.crs.data['init'][5:])
+        
+        coordinates = geojson['features'][0]['geometry']
         
         # Clip the raster with Polygon
-        out_img, out_transform = mask(dataset=stitched_file, shapes=coords, crop=True)
+        out_img, out_transform = mask(dataset=stitched_file, shapes=[coordinates], crop=True)
         
         # Copy the metadata
         out_meta = stitched_file.meta.copy()
-        # print(out_meta)
-
-        epsg_code = int(stitched_file.crs.data['init'][5:])
         
         out_meta.update({"driver": "GTiff",
                 "height": out_img.shape[1],
                 "width": out_img.shape[2],
                 "transform": out_transform,
-                "crs": pycrs.parse.from_epsg_code(epsg_code).to_proj4()}
+                "crs": pycrs.parse.from_epsg_code('3857').to_proj4()}
                         )
         
-        self.setDir('ClippedFiles')
-        with rasterio.open(output_file_name + '_clip.tif', "w", **out_meta) as dest:
+        HelperMethods().setDir('ClippedFiles')
+        
+        with rasterio.open('test' + '_clip.tif', "w", **out_meta) as dest:
             dest.write(out_img)
         
-        clip_file = output_file_name + '_clip.tif'
-        os.chdir('..')
+        # clip_file = output_file_name + '_clip.tif'
+        # os.chdir('..')
         
-        return clip_file
+        # return clip_file
+        
+    def getFeatures(self, gdf):
+        """Function to parse features from GeoDataFrame in such a manner that rasterio wants them"""
+        
+        return [json.loads(gdf.to_json())['features'][0]['geometry']]
