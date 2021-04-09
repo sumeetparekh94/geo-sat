7ddea8d58f22897214ee11bc2b1344e5d18fff2e
sumeetparekh94 <sumeet.parekh1@gmail.com>
2021-04-09 14:28:34 -0400

    added method to parse geojson file

--- src/geo_sat_driver.py
+++ src/geo_sat_driver.py
@@ -4,6 +4,7 @@ import pickle
 import os
 import pandas as pd
 import logging
+import json
 from helper_methods import HelperMethods
 from extract_mbtiles import ExtractMBTiles
 from georeference_image import GeoreferenceImage
@@ -12,53 +13,32 @@ from clip_raster import ClipRaster
 from translate_image import TranslateImage
 
 class Driver(object):
+  def __init__(self):
+    self.geojson_path = '../data/test.geojson'
+  
+  
+  def parse_geojson(self):
+    with open(self.geojson_path) as geojson:
+      data = json.load(geojson)
+      
+    geojson = data['features'][0]['geometry']
     
-    def run_geo_sat_driver(self, geojson):
-      
-      unique_id, output_file_name = HelperMethods().generate_unique_id_file()
-      
-      # Clip area of interest by passing geojson as argument
-      ClipRaster().clip_aoi(geojson, unique_id, output_file_name)
-      
-      # Translate image into png
-      TranslateImage('PNG').translate_image()
+    return geojson
+  
+  def run_geo_sat_driver(self):
+    
+    geojson = Driver().parse_geojson()
+    
+    unique_id, output_file_name = HelperMethods().generate_unique_id_file()
+    
+    # Clip area of interest by passing geojson as argument
+    ClipRaster().clip_aoi(geojson, unique_id, output_file_name)
+    
+    # Translate image into png
+    TranslateImage('PNG').translate_image()
 
 if __name__ == '__main__':
-    geojson = {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              -71.09115600585936,
-              42.37858270194822
-            ],
-            [
-              -71.08634948730469,
-              42.356514317057886
-            ],
-            [
-              -71.02832794189452,
-              42.35930500076174
-            ],
-            [
-              -71.03004455566406,
-              42.379850764344134
-            ],
-            [
-              -71.09115600585936,
-              42.37858270194822
-            ]
-          ]
-        ]
-      }
-    }
-  ]
-}
-    Driver().run_geo_sat_driver(geojson)
+
+  Driver().run_geo_sat_driver()
+    
     
